3064fd23626b52f37910043e72aeb0d21af34678
Prabeen <prabeensoreng2119@gamil.com>
2018-12-07 17:28:19 +0530

    templating engine pug implemented

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoute = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminRoute);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
+    res.status(404).render('not-found', { pageTitle: 'Not Found' });
 });
 
 app.listen(3000);
