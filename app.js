a06a35c8a3cc6f32dd37d48a2923704096d8d7b3
Prabeen <prabeensoreng2119@gamil.com>
2018-12-08 18:00:39 +0530

    implementing MVC architecture

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const error = require('./controllers/not-found');
 
 const app = express();
 
@@ -11,12 +12,9 @@ app.set('views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
-    res.status(404).render('not-found', { pageTitle: 'Not Found', path: '' });
-});
+app.use(error.get404);
 
 app.listen(3000);
