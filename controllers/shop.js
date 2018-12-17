da0222eeef7fd68b05752fe97f249aaa4cbecbba
Prabeen <prabeensoreng2119@gmail.com>
2018-12-17 16:08:17 +0530

    implementing user orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const Order = require('../models/order');
 
 exports.getIndex = (req, res) => {
     Product.findAll()
@@ -87,9 +88,41 @@ exports.postCartDeleteProduct = (req, res) => {
         .catch(err => console.log(err));
 };
 
+exports.postOrder = (req, res) => {
+    let fetchedCart;
+    req.user
+        .getCart()
+        .then(cart => {
+            fetchedCart = cart;
+            return cart.getProducts();
+        })
+        .then(products => {
+            return req.user.createOrder()
+                .then(order => {
+                    return order.addProducts(products.map(product => {
+                        product.orderItem = { quantity: product.cartItem.quantity };
+                        return product;
+                    }));
+                })
+                .catch(err => console.log(err));
+        })
+        .then(result => {
+            return fetchedCart.setProducts(null);
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
+};
+
 exports.getOrders = (req, res) => {
-    res.render('shop/orders', { pageTitle: 'Your Orders', path: '/orders' });
-}
+    req.user
+        .getOrders({ include: ['products'] })
+        .then(orders => {
+            res.render('shop/orders', { pageTitle: 'Your Orders', path: '/orders', orders: orders });
+        })
+        .catch(err => console.log(err));
+};
 
 exports.getCheckout = (req, res) => {
     res.render('shop/checkout', { pageTitle: 'Checkout', path: '/checkout' })
