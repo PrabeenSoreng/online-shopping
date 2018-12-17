60d59fcfb28c4258489244af853f4fed37582818
Prabeen <prabeensoreng2119@gmail.com>
2018-12-17 14:32:19 +0530

    implementing cart functionality

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const error = require('./controllers/not-found');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -31,7 +33,13 @@ app.use(shopRoutes);
 app.use(error.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product); // Optional
+User.hasMany(Product);
+
+Cart.belongsTo(User);
+User.hasOne(Cart);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize.sync() // sync({force: true}) should not be used in production mode.
     .then(result => {
@@ -45,6 +53,9 @@ sequelize.sync() // sync({force: true}) should not be used in production mode.
     })
     .then(user => {
         // console.log(user);
+        return user.createCart();
+    })
+    .then(cart => {
         app.listen(3000);
     })
     .catch(err => console.log(err));
