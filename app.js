8e06f7f7b2587fb2dcb59c443c9e327f0063d89e
Prabeen <prabeensoreng2119@gmail.com>
2018-12-20 17:30:37 +0530

    implementing cart functionality with mongodb

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('5c1b3a334e71c315b4bfaa8b')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
